4ef48569f5b3b5f49d9dcd1e84e891788a1a55a5
vrbaskiz <vrbaskiz@gmail.com>
2025-05-23 16:48:39 +0200

    Add django debug=true

--- stock_market/settings.py
+++ stock_market/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.environ.get('DJANGO_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = os.environ.get('DEBUG', 'False').lower() == 'true'
 
 ALLOWED_HOSTS = []
 
